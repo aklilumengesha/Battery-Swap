aa0e4d9c04db8fa03f010602b1c074b8bdd74ec9
Aklilu Mengesha <aklilumengesha57@gmail.com>
2026-02-26 17:21:39 +0300

    feat: add battery serializers for API

---
+++ backend/battery/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from battery.models import Battery, Station, Vehicle
+
+
+class StationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Station
+        fields = ["pk", "name", "latitude", "longitude", "batteries"]
+
+
+class FindStationSerializer(serializers.ModelSerializer):
+    distance = serializers.SerializerMethodField()
+    # distance = serializers.FloatField()
+    # time = serializers.IntegerField()
+
+    def get_distance(self, obj):
+        return "89"
+
+    class Meta:
+        model = Station
+        fields = ["pk", "name", "distance"]
+
+
+class NewBatteriesSerializer(serializers.Serializer):
+    class Meta:
+        model = Station
+        fields = ["batteries"]
+
+
+class BatterySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Battery
+        fields = ["pk", "vehicle", "company", "price"]
+
+
+class VehicleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vehicle
+        fields = ["pk", "name"]
