625e24a44ddc625c1f3be165af51856d0eb7b6fd
Aklilu Mengesha <aklilumengesha57@gmail.com>
2026-03-13 14:15:26 +0300

    refactor: Update admin registration for CustomUser model

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser, Order
 
-from user.models import Order, User
 
-admin.site.register(User)
+@admin.register(CustomUser)
+class CustomUserAdmin(BaseUserAdmin):
+    ordering = ["email"]
+    list_display = ["email", "name", "user_type", "is_active", "date_joined"]
+    list_filter = ["user_type", "is_active", "is_staff"]
+    search_fields = ["email", "name"]
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("name", "phone", "avatar")}),
+        ("Role", {"fields": ("user_type",)}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Dates", {"fields": ("date_joined", "last_active")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "name", "user_type", "password1", "password2"),
+        }),
+    )
+
+
 admin.site.register(Order)
